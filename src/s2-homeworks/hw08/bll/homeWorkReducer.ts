0b304e783364fb6117145dc4357085457d7dd05d
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2023-06-16 22:14:32 +0300

    Commit HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,22 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return state.map(el => ({...el}))
+                .sort((a,b)=> {
+                    if (a.name > b.name) {
+                        return action.payload === 'up' ? 1 : -1;
+                    } else if (a.name < b.name) {
+                        return action.payload === 'up' ? -1 : 1;
+                    } else {
+                        return 0;
+                    }
+                })
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age >= action.payload)// need to fix
         }
         default:
             return state
