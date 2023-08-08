cb782ba7c2bd3e147cf5ae97d73a61d047ab4a85
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2023-08-08 22:33:31 +0300

    Commit HW12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,24 @@
+
+type InitStatePropsType = {
+    themeId: number
+}
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeIdPropsType): InitStatePropsType => { // fix any
     switch (action.type) {
-        // дописать
-
+        case 'SET_THEME_ID':
+            return {...state, themeId: action.id};
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+
+type ChangeThemeIdPropsType = {
+    type: 'SET_THEME_ID'
+    id: number
+}
+export const changeThemeId = (id: number): ChangeThemeIdPropsType => ({ type: 'SET_THEME_ID', id }) // fix any
